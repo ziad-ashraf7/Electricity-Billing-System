2305a0a00e464271821ecba128470e2a76e1f490
Ziad Ashraf <ziad.ashraf2027@gmail.com>
2024-12-07 20:34:09 +0200

    Added the startup file 'Hello JavaFX'

---
+++ Elictracity Billing System/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module ziad.elictracitybillingsystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens ziad.elictracitybillingsystem to javafx.fxml;
+    exports ziad.elictracitybillingsystem;
+}
