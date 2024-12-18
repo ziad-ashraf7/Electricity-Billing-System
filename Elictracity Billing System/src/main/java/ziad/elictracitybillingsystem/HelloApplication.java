65911be79e170328a6729030c465420b8e0398a1
zeezo679 <ziadabdalla105@gmail.com>
2024-12-18 23:19:58 +0200

    Added components of the BillDetails

--- Elictracity Billing System/src/main/java/ziad/elictracitybillingsystem/HelloApplication.java
+++ Elictracity Billing System/src/main/java/ziad/elictracitybillingsystem/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("RegisterPage.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("BillDetails.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
