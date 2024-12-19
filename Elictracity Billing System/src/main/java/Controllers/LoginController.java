50d49b2918ad527b439cbd9f8a467256d622fca4
HamzaDev <hmzt25235@gmail.com>
2024-12-19 09:45:47 +0200

    fixes

--- Elictracity Billing System/src/main/java/Controllers/LoginController.java
+++ Elictracity Billing System/src/main/java/Controllers/LoginController.java
@@ -27,28 +27,33 @@ public class LoginController implements Initializable {
         List<Complaint> complaintsTmp = new ArrayList<Complaint>();
         String[] compRecord = new String[0];
 
-        fscanner = new Scanner(new File(String.valueOf(UserUnitTesting.class.getResource("/Database/Complaints.csv")).replace("file:/", "").replace("%20"," ")));
-        while(fscanner.hasNext()){
-            compRecord = fscanner.next().split(",");
-            if(compRecord[0].equals(recordArr[0])){
-                complaintsTmp.add(new Complaint(compRecord[0], compRecord[1], 1));
+        try{
+            fscanner = new Scanner(new File(String.valueOf(UserUnitTesting.class.getResource("/Database/Complaints.csv")).replace("file:/", "").replace("%20"," ")));
+            while(fscanner.hasNext()){
+                compRecord = fscanner.next().split(",");
+                if(compRecord[0].equals(recordArr[0])){
+                    complaintsTmp.add(new Complaint(compRecord[0], compRecord[1]));
+                }
             }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
 
         List<String> unpaidBillsTmp = new ArrayList<String>();
         List<String> BillsTmp = new ArrayList<String>();
-        fscanner = new Scanner(new File(String.valueOf(UserUnitTesting.class.getResource("/Database/Bills.csv")).replace("file:/", "").replace("%20"," ")));
-        while(fscanner.hasNext()){
-            compRecord = fscanner.next().split(",");
-            if(compRecord[1].equals(recordArr[0])){
-                BillsTmp.add(compRecord[0]);
-                if(compRecord[6].equals("unpaid")){
-                    unpaidBillsTmp.add(compRecord[0]);
+        try {
+            fscanner = new Scanner(new File(String.valueOf(UserUnitTesting.class.getResource("/Database/Bills.csv")).replace("file:/", "").replace("%20"," ")));
+            while(fscanner.hasNext()){
+                compRecord = fscanner.next().split(",");
+                if(compRecord[1].equals(recordArr[0])){
+                    BillsTmp.add(compRecord[0]);
+                    if(compRecord[6].equals("unpaid")){
+                        unpaidBillsTmp.add(compRecord[0]);
+                    }
                 }
             }
-        }
-        new OldCustomer(Integer.parseInt(recordArr[0]), recordArr[1], recordArr[3], recordArr[4], recordArr[5], recordArr[6], complaintsTmp, unpaidBillsTmp, BillsTmp);
-        return null;
+        } catch (FileNotFoundException e) {}
+        return new OldCustomer(Integer.parseInt(recordArr[0]), recordArr[1], recordArr[3], recordArr[4], recordArr[5], recordArr[6], complaintsTmp, unpaidBillsTmp, BillsTmp);
     }
     static Admin parseAdminInfoObject(String[] recordArr) throws FileNotFoundException {
 //        return new Admin(Integer.parseInt(recordArr[0]), recordArr[1], recordArr[2], recordArr[3]);
@@ -83,6 +88,8 @@ public class LoginController implements Initializable {
                 String record = fscanner.next();
                 recordArr = record.split(",");
 
+                Login x = new Login(parseCustomerInfoObject(recordArr));
+
                 if(email.equals(recordArr[1]) && password.equals(recordArr[2])){
                     Stage cstg = (Stage)loginBtn.getScene().getWindow();
                     cstg.close();
@@ -96,7 +103,7 @@ public class LoginController implements Initializable {
                     stage.setResizable(true);
 
                     stage.show();
-                    return new Login(parseCustomerInfoObject(recordArr));
+                    return x;
                 }
             }
         } else if(accType.equals("Admin")){
